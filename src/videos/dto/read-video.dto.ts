fe7d1be37741430a934d4ca9463dd9304ca43b03
vincent687 <49087520+vincent687@users.noreply.github.com>
2022-01-30 22:49:30 +0800

    Add thumbnail in video

---
+++ src/videos/dto/read-video.dto.ts
@@ -0,0 +1,13 @@
+import { Channel } from "src/channels/entities/channel.entity";
+import { User } from "src/users/entities/user.entity";
+
+export class ReadVideoDto {
+  id: number;
+  name: string;
+  description: string;
+  slide_type: string;
+  url: string;
+  thumbnail: string;
+  owner: User;
+  channel: Channel;
+}
--- src/videos/videos.service.ts
+++ src/videos/videos.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Channel } from "src/channels/entities/channel.entity";
 import { Repository } from "typeorm";
 import { CreateVideoDto } from "./dto/create-video.dto";
+import { ReadVideoDto } from "./dto/read-video.dto";
 import { UpdateVideoDto } from "./dto/update-video.dto";
 import { Video } from "./entities/video.entity";
 
@@ -16,29 +18,78 @@ export class VideosService {
     return "This action adds a new video";
   }
 
-  findAll() {
-    return this.VideosRepository.createQueryBuilder("video")
+  async findAll() {
+    let videos = await this.VideosRepository.createQueryBuilder("video")
       .leftJoinAndSelect("video.owner", "user")
       .leftJoinAndSelect("user.partner", "partner")
       .leftJoinAndSelect("video.channel", "channel")
       .getMany();
+
+    const result: ReadVideoDto[] = videos.map((key, index) => {
+      let thumbnail = "";
+      if (key.url) {
+        if (key.url.includes("youtu.be")) {
+          const segemnts = key.url.split("/");
+          const last = segemnts.pop() || segemnts.pop();
+          thumbnail = `https://img.youtube.com/vi/${last}/0.jpg`;
+        }
+      }
+      const obj: ReadVideoDto = {
+        ...key,
+        thumbnail: thumbnail,
+      };
+      return obj;
+    });
+    return result;
   }
 
-  findAllByCourseId(id: number) {
-    return this.VideosRepository.createQueryBuilder("video")
+  async findAllByCourseId(id: number) {
+    let videos = await this.VideosRepository.createQueryBuilder("video")
       .leftJoinAndSelect("video.owner", "user")
       .leftJoinAndSelect("user.partner", "partner")
       .where("video.channel_id = :id", { id })
       .getMany();
+
+    const result: ReadVideoDto[] = videos.map((key, index) => {
+      let thumbnail = "";
+      if (key.url) {
+        if (key.url.includes("youtu.be")) {
+          const segemnts = key.url.split("/");
+          const last = segemnts.pop() || segemnts.pop();
+          thumbnail = `https://img.youtube.com/vi/${last}/0.jpg`;
+        }
+      }
+      const obj: ReadVideoDto = {
+        ...key,
+        thumbnail: thumbnail,
+      };
+      return obj;
+    });
+    return result;
   }
 
-  findOne(id: number) {
-    return this.VideosRepository.createQueryBuilder("video")
+  async findOne(id: number) {
+    let video = await this.VideosRepository.createQueryBuilder("video")
       .leftJoinAndSelect("video.owner", "user")
       .leftJoinAndSelect("user.partner", "partner")
       .leftJoinAndSelect("video.channel", "channel")
       .where("video.id = :id", { id })
       .getOne();
+
+    let thumbnail = "";
+    if (video.url) {
+      if (video.url.includes("youtu.be")) {
+        const segemnts = video.url.split("/");
+        const last = segemnts.pop() || segemnts.pop();
+        thumbnail = `https://img.youtube.com/vi/${last}/0.jpg`;
+      }
+    }
+
+    const obj: ReadVideoDto = {
+      ...video,
+      thumbnail: thumbnail,
+    };
+    return obj;
   }
 
   update(id: number, updateVideoDto: UpdateVideoDto) {
