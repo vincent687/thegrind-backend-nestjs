ba3f58907dcbf254565a9dba48189b32f0f66e4f
vincent687 <49087520+vincent687@users.noreply.github.com>
2022-05-21 17:10:51 +0800

    Update course api

--- src/non-odoo/courses/courses.service.ts
+++ src/non-odoo/courses/courses.service.ts
@@ -21,27 +21,39 @@ export class CoursesService {
       location: createCourseDto.location,
       createdby_user: createCourseDto.createdby_user,
       created_date: createCourseDto.created_date,
+      companyId: createCourseDto.companyId,
+      course_email: createCourseDto.course_email,
     };
 
     const newCourse = await this.CoursesRepository.create(course);
-    const tutorArray = await createCourseDto.tutors.reduce((acc, val) => {
-      return acc.concat({
-        cid: newCourse.id,
-        user_id: val,
-      });
-    }, []);
-    const studentArray = await createCourseDto.students.reduce((acc, val) => {
-      return acc.concat({
-        cid: newCourse.id,
-        user_id: val,
-      });
-    }, []);
-    const tagArray = await createCourseDto.courseTags.reduce((acc, val) => {
-      return acc.concat({
-        cid: newCourse.id,
-        tag_id: val,
-      });
-    }, []);
+
+    const tutorArray =
+      createCourseDto.tutors != null
+        ? await createCourseDto.tutors.reduce((acc, val) => {
+            return acc.concat({
+              cid: newCourse.id,
+              user_id: val,
+            });
+          }, [])
+        : [];
+    const studentArray =
+      createCourseDto.students != null
+        ? await createCourseDto.students.reduce((acc, val) => {
+            return acc.concat({
+              cid: newCourse.id,
+              user_id: val,
+            });
+          }, [])
+        : [];
+    const tagArray =
+      createCourseDto.courseTags != null
+        ? await createCourseDto.courseTags.reduce((acc, val) => {
+            return acc.concat({
+              cid: newCourse.id,
+              tag_id: val,
+            });
+          }, [])
+        : [];
 
     let entity2 = {
       ...newCourse,
--- src/non-odoo/courses/dto/update-course.dto.ts
+++ src/non-odoo/courses/dto/update-course.dto.ts
@@ -1,4 +1,17 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCourseDto } from './create-course.dto';
+import { PartialType } from "@nestjs/swagger";
+import { CreateCourseDto } from "./create-course.dto";
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {
+  name: string;
+  description: string;
+  start_date: Date;
+  end_date: Date;
+  location: string;
+  createdby_user: number;
+  created_date: Date;
+  tutors: number[];
+  students: number[];
+  courseTags: number[];
+  companyId: number;
+  course_email: string;
+}
